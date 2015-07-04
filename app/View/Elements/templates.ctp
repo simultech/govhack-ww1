eba80a25f1f055c9cefce78ae4be0f9b696fce41
Andrew Dekker <simultech@gmail.com>
2015-07-05 09:44:53 +1000

    posters

--- app/View/Elements/templates.ctp
+++ app/View/Elements/templates.ctp
@@ -19,3 +19,27 @@
 		</div>
 	</section>
 </script>
+
+<script id="el-poster" type="text/x-handlebars-template">
+	
+<section>
+	<div class="container">
+		<div>
+			<div class="col-sm-6 col-md-7">
+				<h3 class = "header">{{title}}</h3>
+				<p>{{description}}</p>
+				<p>{{creator}}</p>
+				<ul class="list-icon">
+					<li>Published: {{d}}</li>
+				</ul>
+			</div>
+			<div class="col-sm-6 col-md-5">
+				<div>
+					<div>
+						<img alt="" class="img-responsive" src="{{identifier}}">
+					</div>
+				</div>
+			</div>
+	</div>
+</section>
+</script>
--- app/webroot/js/api_calls.js
+++ app/webroot/js/api_calls.js
@@ -1,7 +1,8 @@
 
 $('document').ready(function() {
     console.log("STARTING");
-    getsoldierportraits();
+    //getsoldierportraits();
+    getposters();
 });
 
 function getsoldierportraits() {
@@ -15,6 +16,22 @@ function getsoldierportraits() {
     		var source   = $("#el-soldier").html();
 			var template = Handlebars.compile(source);
 			var context = {title: stories[story]['Qldsoldierportrait']['title'], body: stories[story]['Qldsoldierportrait']['description'], img: stories[story]['Qldsoldierportrait']['500pixel']};
+			var html    = template(context);
+	    	$('#koko').append($(html));
+    	}
+	});
+}
+
+function getposters() {
+	var url = 'http://atwarandathome.com/api/posters';
+	$.ajax({
+		url: url,
+	})
+	.done(function(stories) {
+    	for(var story in stories) {
+    		var source   = $("#el-poster").html();
+			var template = Handlebars.compile(source);
+			var context = stories[story]['posters'];
 			var html    = template(context);
 	    	$('#koko').append($(html));
     	}
